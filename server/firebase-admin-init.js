8174e9bed8d4d9d7a61c972f12dbb39285b98506
sahil <sahilshaikhts@gmail.com>
2023-11-02 00:35:41 -0400

    fix env file

--- server/firebase-admin-init.js
+++ server/firebase-admin-init.js
@@ -1,9 +1,4 @@
 //if deployed use env from host settings
-if (process.env.NODE_ENV == 'production') {
-    require('dotenv').config({path:'/etc/secrets/.env'});
-} else {
-    require('dotenv').config({ path: './server/config/.env' });
-}
 const admin=require('firebase-admin');
 admin.initializeApp({
     credential:admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN_KEY)),
--- server/server.js
+++ server/server.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const { VerifyAccessToken, VerifyRefreshToken } = require("./Middleware/ValidateToken");
-//if deployed use env from host settings
-if (process.env.NODE_ENV == 'production') {
-    require('dotenv').config({path:'/etc/secrets/.env'});
-} else {
-    require('dotenv').config({ path: './server/config/.env' });
-}
+require('dotenv').config();
+
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const app = express();
