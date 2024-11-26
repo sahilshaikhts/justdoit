a88ead5671f8770e24fa25f3ddf056092d8c7782
sahil <sahilshaikhts@gmail.com>
2024-11-25 21:20:55 -0500

    Bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app.jsx'
-import { UserAuthProvider } from './Context/AuthorizationContext.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app.jsx";
+import { UserAuthProvider } from "./Context/AuthorizationContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <UserAuthProvider>
-            <App />
-        </UserAuthProvider>
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <UserAuthProvider>
+      <App />
+    </UserAuthProvider>
+  </React.StrictMode>
 );
-
